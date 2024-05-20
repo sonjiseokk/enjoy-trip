8ee462b91b9758b378dda5f945087c7c59c1223a
thswltjr11 <thswltjr11@gmail.com>
2024-05-20 21:05:44 +0900

    test: 캐시 삭제

--- src/main/java/com/ssafy/enjoytrip/global/exception/DuplicateNameException.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.enjoytrip.global.exception;
-
-public class DuplicateNameException extends Exception {
-    public DuplicateNameException(final String message) {
-        super(message);
-    }
-}
